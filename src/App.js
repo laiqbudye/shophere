4f793ead4208226bae2b4abe1256182d2a5d52ba
laiqb <laiqbudye@gmail.com>
2022-04-05 00:57:27 +0530

    react bootstrap || added react bootstrap dependency & themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1> Welcome to ShopHere </h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
